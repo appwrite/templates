7856b2c8820e60a1bd9ef33a91643ea29c7056a6
loks0n <22452787+loks0n@users.noreply.github.com>
2023-07-11 16:10:19 +0100

    chore: add prettier

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -4,19 +4,17 @@ const path = require('path')
 const staticFolder = path.join(__dirname, '../static')
 
 module.exports = async ({ req, res, log }) => {
-    log('Hello, World!')
+  log('Hello, World!')
 
-    if (req.method === 'GET') {
-        let html = fs
-            .readFileSync(path.join(staticFolder, 'index.html'))
-            .toString()
+  if (req.method === 'GET') {
+    let html = fs.readFileSync(path.join(staticFolder, 'index.html')).toString()
 
-        return res.send(html, 200, {
-            'Content-Type': 'text/html; charset=utf-8',
-        })
-    }
-
-    return res.json({
-        message: 'Hello, World!',
+    return res.send(html, 200, {
+      'Content-Type': 'text/html; charset=utf-8',
     })
+  }
+
+  return res.json({
+    message: 'Hello, World!',
+  })
 }
