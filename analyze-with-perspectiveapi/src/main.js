0d067d1183e554562a82b5599d5b75143106c8a1
loks0n <22452787+loks0n@users.noreply.github.com>
2023-07-19 10:57:23 +0100

    chore: add semis, del pjson extras

--- analyze-with-perspectiveapi/src/main.js
+++ analyze-with-perspectiveapi/src/main.js
@@ -1,28 +1,28 @@
-import fs from 'node:fs'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { fetch } from 'undici'
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { fetch } from 'undici';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-const staticFolder = path.join(__dirname, '../static')
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const staticFolder = path.join(__dirname, '../static');
 
 export default async ({ req, res }) => {
-  const { PERSPECTIVE_API_KEY } = process.env
+  const { PERSPECTIVE_API_KEY } = process.env;
 
   if (!PERSPECTIVE_API_KEY) {
-    throw new Error('Function is missing required environment variables.')
+    throw new Error('Function is missing required environment variables.');
   }
 
   if (req.method === 'GET') {
     const html = fs
       .readFileSync(path.join(staticFolder, 'index.html'))
-      .toString()
-    return res.send(html, 200, { 'Content-Type': 'text/html; charset=utf-8' })
+      .toString();
+    return res.send(html, 200, { 'Content-Type': 'text/html; charset=utf-8' });
   }
 
   if (!req.bodyString) {
-    return res.send('Missing body with a prompt.', 400)
+    return res.send('Missing body with a prompt.', 400);
   }
 
   const response = await fetch(
@@ -43,12 +43,12 @@ export default async ({ req, res }) => {
         },
       }),
     }
-  )
+  );
 
   if (response.status !== 200) {
-    return res.send('Error analyzing text.', 500)
+    return res.send('Error analyzing text.', 500);
   }
 
-  const data = /** @type {*} */ (await response.json())
-  return res.send(data.attributeScores.TOXICITY.summaryScore.value)
-}
+  const data = /** @type {*} */ (await response.json());
+  return res.send(data.attributeScores.TOXICITY.summaryScore.value);
+};
