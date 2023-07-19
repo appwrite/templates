76228cd0ebb10f00af7401e4d8f069e4d54395f3
loks0n <22452787+loks0n@users.noreply.github.com>
2023-07-19 10:30:54 +0100

    chore: add semis

--- hello-world/src/main.js
+++ hello-world/src/main.js
@@ -1,24 +1,24 @@
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const staticFolder = path.join(__dirname, '../static')
+const staticFolder = path.join(__dirname, '../static');
 
 export default async ({ req, res, log, error }) => {
-  log('Hello, Logs! 👋')
-  error('Hello, Errors! 👋')
+  log('Hello, Logs! 👋');
+  error('Hello, Errors! 👋');
 
   if (req.method === 'GET') {
     const html = fs
       .readFileSync(path.join(staticFolder, 'index.html'))
-      .toString()
-    return res.send(html, 200, { 'Content-Type': 'text/html; charset=utf-8' })
+      .toString();
+    return res.send(html, 200, { 'Content-Type': 'text/html; charset=utf-8' });
   }
 
   return res.json({
     message: 'Hello, World! 👋',
-  })
-}
+  });
+};
