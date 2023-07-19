ae349ed6df892c1eaeb7a38988eaedb4a699a18d
loks0n <22452787+loks0n@users.noreply.github.com>
2023-07-19 20:02:24 +0100

    feat: Starter Template function

---
+++ starter-template/README.md
@@ -0,0 +1,34 @@
+# ⚡ Starter Function
+
+A simple starter function. Edit `src/index.js` to get started and create something awesome! 🚀
+
+## 🧰 Usage
+
+### `GET`
+
+- Returns a "Hello, World!" message.
+
+**Response**
+
+Sample `200` Response:
+
+```text
+Hello, World! 🌎
+```
+
+### `POST`, `PUT`, `PATCH`, `DELETE`
+
+- Returns a "Learn More" JSON response.
+
+**Response**
+
+Sample `200` Response:
+
+```json
+{
+  "motto": "Build Fast. Scale Big. All in One Place.",
+  "learn": "https://appwrite.io/docs",
+  "connect": "https://appwrite.io/discord",
+  "getInspired": "https://builtwith.appwrite.io"
+}
+```
---
+++ starter-template/src/main.js
@@ -0,0 +1,33 @@
+import { Client } from "node-appwrite";
+
+// This is your Appwrite function
+// It's executed each time we get a request
+export default async ({ req, res, log, error }) => {
+  // Why not try the Appwrite SDK?
+  //
+  // const client = new Client()
+  //    .setEndpoint(process.env.APPWRITE_ENDPOINT)
+  //    .setProject(process.env.APPWRITE_PROJECT_ID)
+  //    .setKey(process.env.APPWRITE_API_KEY);
+
+  // You can log messages to the console
+  log("Hello, Logs! 👋");
+
+  // If something goes wrong, log an error
+  error("Hello, Errors! ⛔");
+
+  // The `req` object contains the request data
+  if (req.method === "GET") {
+    // Send a response with the res object helpers
+    // `res.send()` dispatches a string back to the client
+    return res.send("Hello, World! 🌎");
+  }
+
+  // `res.json()` is a handy helper for sending JSON
+  return res.json({
+    motto: "Build Fast. Scale Big. All in One Place.",
+    learn: "https://appwrite.io/docs",
+    connect: "https://appwrite.io/discord",
+    getInspired: "https://builtwith.appwrite.io",
+  });
+};
