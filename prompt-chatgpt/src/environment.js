124c53879f72edcf87cadac57dc109d378c139cc
loks0n <22452787+loks0n@users.noreply.github.com>
2023-07-19 11:03:46 +0100

    chore: add semis, del pjson extras

--- prompt-chatgpt/src/environment.js
+++ prompt-chatgpt/src/environment.js
@@ -3,11 +3,11 @@
  * @return {string}
  */
 function getRequiredEnv(key) {
-  const value = process.env[key]
+  const value = process.env[key];
   if (value === undefined) {
-    throw new Error(`Environment variable ${key} is not set`)
+    throw new Error(`Environment variable ${key} is not set`);
   }
-  return value
+  return value;
 }
 
 /**
@@ -15,15 +15,15 @@ function getRequiredEnv(key) {
  * @return {number | undefined}
  */
 function getNumberEnv(key) {
-  const value = process.env[key]
+  const value = process.env[key];
   if (value === undefined) {
-    return undefined
+    return undefined;
   }
 
   try {
-    return parseInt(value)
+    return parseInt(value);
   } catch (e) {
-    throw new Error(`Environment variable ${key} is not a number`)
+    throw new Error(`Environment variable ${key} is not a number`);
   }
 }
 
@@ -31,7 +31,7 @@ function EnvironmentService() {
   return {
     OPENAI_API_KEY: getRequiredEnv('OPENAI_API_KEY'),
     OPENAI_MAX_TOKENS: getNumberEnv('OPENAI_MAX_TOKENS') ?? 64,
-  }
+  };
 }
 
-export default EnvironmentService
+export default EnvironmentService;
