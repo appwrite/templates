adf1d6a2dd2f6d223c13dcfa06aa8c9946064920
loks0n <22452787+loks0n@users.noreply.github.com>
2023-07-19 11:04:49 +0100

    chore: add semis, del pjson extras

--- sync-with-algolia/src/main.js
+++ sync-with-algolia/src/main.js
@@ -1,14 +1,13 @@
-import { Client, Databases, Query } from 'node-appwrite'
-import algoliasearch from 'algoliasearch'
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import EnvironmentService from './environment.js'
+import { Client, Databases, Query } from 'node-appwrite';
+import algoliasearch from 'algoliasearch';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import EnvironmentService from './environment.js';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-const staticFolder = path.join(__dirname, '../static')
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const staticFolder = path.join(__dirname, '../static');
 
 /**
  * @param {string} template
@@ -16,7 +15,7 @@ const staticFolder = path.join(__dirname, '../static')
  * @returns {string}
  */
 function interpolate(template, values) {
-  return template.replace(/{{([^}]+)}}/g, (_, key) => values[key])
+  return template.replace(/{{([^}]+)}}/g, (_, key) => values[key]);
 }
 
 export default async ({ req, res, log }) => {
@@ -30,65 +29,65 @@ export default async ({ req, res, log }) => {
     APPWRITE_COLLECTION_ID,
     APPWRITE_ENDPOINT,
     APPWRITE_PROJECT_ID,
-  } = EnvironmentService()
+  } = EnvironmentService();
 
   if (req.method === 'GET') {
     const template = fs
       .readFileSync(path.join(staticFolder, 'index.html'))
-      .toString()
+      .toString();
 
     const html = interpolate(template, {
       ALGOLIA_APP_ID,
       ALGOLIA_INDEX_ID,
       ALGOLIA_SEARCH_API_KEY,
-    })
+    });
 
-    return res.send(html, 200, { 'Content-Type': 'text/html; charset=utf-8' })
+    return res.send(html, 200, { 'Content-Type': 'text/html; charset=utf-8' });
   }
 
   const client = new Client()
     .setEndpoint(APPWRITE_ENDPOINT)
     .setProject(APPWRITE_PROJECT_ID)
-    .setKey(APPWRITE_API_KEY)
+    .setKey(APPWRITE_API_KEY);
 
-  const databases = new Databases(client)
+  const databases = new Databases(client);
 
-  const algolia = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_API_KEY)
-  const algoliaIndex = algolia.initIndex(ALGOLIA_INDEX_ID)
+  const algolia = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_API_KEY);
+  const algoliaIndex = algolia.initIndex(ALGOLIA_INDEX_ID);
 
-  let cursor = null
+  let cursor = null;
 
   do {
-    const queries = [Query.limit(100)]
+    const queries = [Query.limit(100)];
 
     if (cursor) {
-      queries.push(Query.cursorAfter(cursor))
+      queries.push(Query.cursorAfter(cursor));
     }
 
     const response = await databases.listDocuments(
       APPWRITE_DATABASE_ID,
       APPWRITE_COLLECTION_ID,
       queries
-    )
+    );
 
     if (response.documents.length > 0) {
-      cursor = response.documents[response.documents.length - 1].$id
+      cursor = response.documents[response.documents.length - 1].$id;
     } else {
-      log(`No more documents found.`)
-      cursor = null
-      break
+      log(`No more documents found.`);
+      cursor = null;
+      break;
     }
 
-    log(`Syncing chunk of ${response.documents.length} documents ...`)
+    log(`Syncing chunk of ${response.documents.length} documents ...`);
 
     const records = response.documents.map((document) => ({
       ...document,
       objectID: document.$id,
-    }))
-    await algoliaIndex.saveObjects(records)
-  } while (cursor !== null)
+    }));
+    await algoliaIndex.saveObjects(records);
+  } while (cursor !== null);
 
-  log('Sync finished.')
+  log('Sync finished.');
 
-  return res.empty()
-}
+  return res.empty();
+};
