e0a0a9986dd7b5ee81dc3399283ff0b6be8bdab6
loks0n <22452787+loks0n@users.noreply.github.com>
2023-07-19 10:58:25 +0100

    chore: add semis, del pjson extras

--- censor-with-redact/src/main.js
+++ censor-with-redact/src/main.js
@@ -1,28 +1,27 @@
-import fs from 'fs'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-const staticFolder = path.join(__dirname, '../static')
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const staticFolder = path.join(__dirname, '../static');
 
 export default async ({ req, res }) => {
-  const { PANGEA_REDACT_TOKEN } = process.env
+  const { PANGEA_REDACT_TOKEN } = process.env;
 
   if (!PANGEA_REDACT_TOKEN) {
-    throw new Error('Function is missing required environment variables.')
+    throw new Error('Function is missing required environment variables.');
   }
 
   if (req.method === 'GET') {
     const html = fs
       .readFileSync(path.join(staticFolder, 'index.html'))
-      .toString()
-    return res.send(html, 200, { 'Content-Type': 'text/html; charset=utf-8' })
+      .toString();
+    return res.send(html, 200, { 'Content-Type': 'text/html; charset=utf-8' });
   }
 
   if (!req.bodyString) {
-    return res.send('Missing body with a prompt.', 400)
+    return res.send('Missing body with a prompt.', 400);
   }
 
   const response = await fetch(`https://redact.aws.eu.pangea.cloud/v1/redact`, {
@@ -34,8 +33,8 @@ export default async ({ req, res }) => {
     body: JSON.stringify({
       text: req.bodyString,
     }),
-  })
+  });
 
-  const data = /** @type {*} */ (await response.json())
-  return res.send(data.result.redacted_text)
-}
+  const data = /** @type {*} */ (await response.json());
+  return res.send(data.result.redacted_text);
+};
