d594634f3d65b0a04609b26c20bae95d6422f6a8
loks0n <22452787+loks0n@users.noreply.github.com>
2023-07-28 11:52:15 +0100

    chore: better

--- node/prompt-chatgpt/src/utils.js
+++ node/prompt-chatgpt/src/utils.js
@@ -4,9 +4,13 @@
  * @param {string[]} keys
  */
 export function throwIfMissing(obj, keys) {
+  const missing = [];
   for (let key of keys) {
     if (!(key in obj) || !obj[key]) {
-      throw new Error(`Missing required value: ${key}`);
+      missing.push(key);
     }
+  }
+  if (missing.length > 0) {
+    throw new Error(`Missing required fields: ${missing.join(', ')}`);
   }
 }
