8b8ad54924c5dd1d77c105e7e20ac1a5e73c0352
loks0n <22452787+loks0n@users.noreply.github.com>
2023-07-31 18:38:52 +0100

    chore: remove unneccessary types

--- node/sync-with-meilisearch/src/main.js
+++ node/sync-with-meilisearch/src/main.js
@@ -26,8 +26,8 @@ export default async ({ req, res, log }) => {
     .setEndpoint(
       process.env.APPWRITE_ENDPOINT ?? 'https://cloud.appwrite.io/v1'
     )
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID ?? '')
-    .setKey(process.env.APPWRITE_API_KEY ?? '');
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
 
   const databases = new Databases(client);
 
