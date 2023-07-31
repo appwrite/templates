940030a9bc31f2ecf7a5cc49acc6685f5c54b0b4
loks0n <22452787+loks0n@users.noreply.github.com>
2023-07-31 20:17:11 +0100

    chore: fix env types

--- node/sync-with-algolia/env.d.ts
+++ node/sync-with-algolia/env.d.ts
@@ -1,14 +1,14 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      ALGOLIA_APP_ID?: string;
-      ALGOLIA_ADMIN_API_KEY?: string;
-      ALGOLIA_INDEX_NAME?: string;
+      ALGOLIA_APP_ID: string;
+      ALGOLIA_ADMIN_API_KEY: string;
+      ALGOLIA_INDEX_ID: string;
       APPWRITE_ENDPOINT?: string;
-      APPWRITE_API_KEY?: string;
+      APPWRITE_API_KEY: string;
       APPWRITE_FUNCTION_PROJECT_ID?: string;
-      APPWRITE_DATABASE_ID?: string;
-      APPWRITE_COLLECTION_ID?: string;
+      APPWRITE_DATABASE_ID: string;
+      APPWRITE_COLLECTION_ID: string;
     }
   }
 }
