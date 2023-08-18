5501ea0541d8d4da872d3dcee85acc09be7c8df7
loks0n <22452787+loks0n@users.noreply.github.com>
2023-08-18 10:16:34 +0100

    feat: prompt_chatgpt dart

---
+++ dart/prompt_chatgpt/lib/main.dart
@@ -0,0 +1,40 @@
+import 'dart:io';
+import 'dart:async';
+import 'package:dart_openai/dart_openai.dart';
+import 'utils.dart';
+
+Future<dynamic> main(final context) async {
+  throwIfMissing(Platform.environment, ['OPENAI_API_KEY']);
+
+  if (context.req.method == 'GET') {
+    return context.res.send(getStaticFile('index.html'), 200,
+        {'Content-Type': 'text/html; charset=utf-8'});
+  }
+
+  try {
+    throwIfMissing(context.req.body, ['prompt']);
+  } catch (err) {
+    return context.res.json({'ok': false, 'error': err.toString()});
+  }
+
+  OpenAI.apiKey = Platform.environment['OPENAI_API_KEY']!;
+
+  try {
+    final response = await OpenAI.instance.chat.create(
+      model: 'gpt-3.5-turbo',
+      maxTokens: int.parse(Platform.environment['OPENAI_MAX_TOKENS'] ?? '512'),
+      messages: [
+        OpenAIChatCompletionChoiceMessageModel(
+            content: context.req.body['prompt'],
+            role: OpenAIChatMessageRole.user)
+      ],
+    );
+
+    final completion = response.choices[0].message.content;
+
+    return context.res.json({'ok': true, 'completion': completion}, 200);
+  } on RequestFailedException {
+    return context.res
+        .json({'ok': false, 'error': 'Failed to query model.'}, 500);
+  }
+}
