9be7174fe8ef3724b77acebca7c7ad56f86bee66
loks0n <22452787+loks0n@users.noreply.github.com>
2023-08-20 16:17:37 +0100

    chore: improve python docstrings

--- python/prompt_chatgpt/src/utils.py
+++ python/prompt_chatgpt/src/utils.py
@@ -4,23 +4,31 @@ __dirname = os.path.dirname(os.path.abspath(__file__))
 static_folder = os.path.join(__dirname, "../static")
 
 
-def get_static_file(file_name):
+def get_static_file(file_name: str) -> str:
     """
     Returns the contents of a file in the static folder
-    :param file_name: Name of the file to read
-    :return: Contents of static/{file_name}
+
+    Parameters:
+        file_name (str): Name of the file to read
+
+    Returns:
+        (str): Contents of static/{file_name}
     """
     file_path = os.path.join(static_folder, file_name)
     with open(file_path, "r") as file:
         return file.read()
 
 
-def throw_if_missing(obj, keys):
+def throw_if_missing(obj: object, keys: list[str]) -> None:
     """
     Throws an error if any of the keys are missing from the object
-    :param obj: Object to validate
-    :param keys: List of keys to check
-    :raises ValueError: If any keys are missing
+
+    Parameters:
+        obj (object): Object to check
+        keys (list[str]): List of keys to check
+
+    Raises:
+        ValueError: If any keys are missing
     """
     missing = [key for key in keys if key not in obj or not obj[key]]
     if missing:
