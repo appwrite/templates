cc9e24c4f11d8c010d2d8808203a5fe3d3857172
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2023-10-01 23:24:56 +0530

    new file:   node-typescript/sync-with-meilisearch/.gitignore 	new file:   node-typescript/sync-with-meilisearch/.prettierrc.json 	new file:   node-typescript/sync-with-meilisearch/README.md 	new file:   node-typescript/sync-with-meilisearch/env.d.ts 	new file:   node-typescript/sync-with-meilisearch/package-lock.json 	new file:   node-typescript/sync-with-meilisearch/package.json 	new file:   node-typescript/sync-with-meilisearch/src/main.ts 	new file:   node-typescript/sync-with-meilisearch/src/utils.ts 	new file:   node-typescript/sync-with-meilisearch/static/index.html 	new file:   node-typescript/sync-with-meilisearch/tsconfig.json

---
+++ node-typescript/sync-with-meilisearch/env.d.ts
@@ -0,0 +1,17 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      APPWRITE_ENDPOINT?: string;
+      APPWRITE_API_KEY: string;
+      APPWRITE_FUNCTION_PROJECT_ID: string;
+      APPWRITE_DATABASE_ID: string;
+      APPWRITE_COLLECTION_ID: string;
+      MEILISEARCH_ENDPOINT: string;
+      MEILISEARCH_INDEX_NAME: string;
+      MEILISEARCH_ADMIN_API_KEY: string;
+      MEILISEARCH_SEARCH_API_KEY: string;
+    }
+  }
+}
+
+export {};
---
+++ node-typescript/sync-with-meilisearch/src/main.ts
@@ -0,0 +1,79 @@
+import { Client, Databases, Query } from 'node-appwrite';
+import { getStaticFile, interpolate, throwIfMissing } from './utils.js';
+import { MeiliSearch } from 'meilisearch';
+
+type Context = {
+  req: any;
+  res: any;
+  log: (msg: any) => void;
+  error: (msg: any) => void;
+};
+
+export default async ({ req, res, log } : Context) => {
+  throwIfMissing(process.env, [
+    'APPWRITE_API_KEY',
+    'APPWRITE_DATABASE_ID',
+    'APPWRITE_COLLECTION_ID',
+    'MEILISEARCH_ENDPOINT',
+    'MEILISEARCH_INDEX_NAME',
+    'MEILISEARCH_ADMIN_API_KEY',
+    'MEILISEARCH_SEARCH_API_KEY',
+  ]);
+
+  if (req.method === 'GET') {
+    const html = interpolate(getStaticFile('index.html'), {
+      MEILISEARCH_ENDPOINT: process.env.MEILISEARCH_ENDPOINT,
+      MEILISEARCH_INDEX_NAME: process.env.MEILISEARCH_INDEX_NAME,
+      MEILISEARCH_SEARCH_API_KEY: process.env.MEILISEARCH_SEARCH_API_KEY,
+    });
+
+    return res.send(html, 200, { 'Content-Type': 'text/html; charset=utf-8' });
+  }
+
+  const client = new Client()
+    .setEndpoint(
+      process.env.APPWRITE_ENDPOINT ?? 'https://cloud.appwrite.io/v1'
+    )
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
+
+  const databases = new Databases(client);
+
+  const meilisearch = new MeiliSearch({
+    host: process.env.MEILISEARCH_ENDPOINT,
+    apiKey: process.env.MEILISEARCH_ADMIN_API_KEY,
+  });
+
+  const index = meilisearch.index(process.env.MEILISEARCH_INDEX_NAME);
+
+  let cursor = null;
+
+  do {
+    const queries = [Query.limit(100)];
+
+    if (cursor) {
+      queries.push(Query.cursorAfter(cursor));
+    }
+
+    const { documents } = await databases.listDocuments(
+      process.env.APPWRITE_DATABASE_ID,
+      process.env.APPWRITE_COLLECTION_ID,
+      queries
+    );
+
+    if (documents.length > 0) {
+      cursor = documents[documents.length - 1].$id;
+    } else {
+      log(`No more documents found.`);
+      cursor = null;
+      break;
+    }
+
+    log(`Syncing chunk of ${documents.length} documents ...`);
+    await index.addDocuments(documents, { primaryKey: '$id' });
+  } while (cursor !== null);
+
+  log('Sync finished.');
+
+  return res.send('Sync finished.', 200);
+};
