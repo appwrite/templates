14f66ebe66dc1ebad3064f5e055a0e170602bc10
Luke B. Silver <22452787+loks0n@users.noreply.github.com>
2023-10-02 13:19:27 +0100

    chore: spacing

--- node-typescript/sync-with-meilisearch/src/utils.ts
+++ node-typescript/sync-with-meilisearch/src/utils.ts
@@ -5,7 +5,6 @@ import fs from "fs";
 /**
  * Throws an error if any of the keys are missing from the object 
  */
-
 export function throwIfMissing(obj: any, keys: string[]): void {
   const missing: string[] = [];
   for (let key of keys) {
@@ -25,15 +24,13 @@ const staticFolder = path.join(__dirname, "../static");
 /** 
  * Returns the contents of a file in the static folder.
  */
-
 export function getStaticFile(fileName: string): string {
   return fs.readFileSync(path.join(staticFolder, fileName)).toString();
 }
 
 /**
- *  Replaces all instances of {{key}} in the template with the value of the key in the values object.
+ * Returns the template with the values interpolated.
  */
-
 export function interpolate(template: string, values: Record<string, string | undefined>): string {
   return template.replace(/{{([^}]+)}}/g, (_, key) => values[key] || "");
 }
