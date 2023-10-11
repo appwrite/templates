c751299ee6060c9ced0576e438bfbd1bba3377c2
Kenneth Bass <kenny_bass@outlook.com>
2023-10-10 20:36:37 -0500

    add testing function for deleting documents

---
+++ node/database-cleaner/src/appwrite.js
@@ -0,0 +1,57 @@
+import { Client, Databases, Query } from 'node-appwrite';
+
+class AppwriteService {
+  constructor() {
+    const client = new Client();
+    client
+      .setEndpoint(
+        process.env.APPWRITE_ENDPOINT ?? 'https://cloud.appwrite.io/v1'
+      )
+      .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+      .setKey(process.env.APPWRITE_API_KEY);
+
+    this.databases = new Databases(client);
+  }
+
+  /**
+   * @param {string} databaseId
+   * @param {string} collectionId
+   * @returns {Promise<void>}
+   */
+  async cleanCollection(databaseId, collectionId) {
+    const queries = [Query.orderAsc('$createdAt')];
+    let done = false;
+
+    while (!done) {
+      const documents = await this.databases.listDocuments(
+        databaseId,
+        collectionId,
+        queries
+      );
+
+      for (const document of documents.documents) {
+        const retention = process.env.RETENTION_PERIOD_DAYS ?? 30;
+        const expirationDate = new Date(
+          document.$createdAt.getTime() + retention * 24 * 60 * 60 * 1000
+        );
+        const today = new Date();
+
+        console.log(`Expiration date: ${expirationDate}`);
+
+        if (expirationDate < today) {
+          try {
+            await this.databases.deleteDocument(
+              databaseId,
+              collectionId,
+              document.$id
+            );
+          } catch (err) {
+            throw new Error(err.message);
+          }
+        }
+      }
+    }
+  }
+}
+
+export default AppwriteService;
--- node/database-cleaner/src/main.js
+++ node/database-cleaner/src/main.js
@@ -1,27 +1,12 @@
-import { Client } from 'node-appwrite';
+import AppwriteService from './appwrite';
 
-// This is your Appwrite function
-// It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+  const appwrite = new AppwriteService();
 
-  // You can log messages to the console
-  log('Hello, Logs!');
+  const databaseId = process.env.APPWRITE_DATABASE_ID;
+  const collectionId = process.env.APPWRITE_COLLECTION_ID;
 
-  // If something goes wrong, log an error
-  error('Hello, Errors!');
-
-  // The `req` object contains the request data
-  if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
-  }
+  appwrite.cleanCollection(databaseId, collectionId);
 
   // `res.json()` is a handy helper for sending JSON
   return res.json({
