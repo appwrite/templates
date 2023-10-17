4d69364b6a5b9aa1e0d8d9a66c4914b341480718
vimo <39148877+vimode@users.noreply.github.com>
2023-10-17 10:46:51 +0530

    add github issue bot template

---
+++ node-typescript/github-issue-bot/src/main.ts
@@ -0,0 +1,33 @@
+import GithubService from './github.js';
+import { throwIfMissing } from './utils.js';
+
+type Context = {
+  req: any;
+  res: any;
+  log: (msg: any) => void;
+  error: (msg: any) => void;
+};
+
+export default async ({ res, req, log, error }: Context) => {
+  throwIfMissing(process.env, ['GITHUB_WEBHOOK_SECRET', 'GITHUB_TOKEN']);
+
+  const github = new GithubService();
+
+  if (!(await github.verifyWebhook(req))) {
+    error('Invalid signature');
+    return res.json({ ok: false, error: 'Invalid signature' }, 401);
+  }
+
+  if (!github.isIssueOpenedEvent(req)) {
+    log('Received non-issue event - ignoring');
+    return res.json({ ok: true });
+  }
+
+  await github.postComment(
+    req.body.repository,
+    req.body.issue,
+    `Thanks for the issue report @${req.body.issue.user.login}! We will look into it as soon as possible.`
+  );
+
+  return res.json({ ok: true });
+};
---
+++ node-typescript/github-issue-bot/src/utils.ts
@@ -0,0 +1,14 @@
+/* Throws an error if any of the keys are missing from the object. */
+
+export function throwIfMissing(obj: any, keys: string[]): void {
+  const missing: string[] = [];
+  for (let key of keys) {
+    if (!(key in obj) || !obj[key]) {
+      missing.push(key);
+    }
+  }
+
+  if (missing.length > 0) {
+    throw new Error(`Missing required fields: ${missing.join(', ')}`);
+  }
+}
