b5799b03d9d93add17f83e547d35353f6cdca629
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2023-10-26 11:25:43 -0500

    fix

--- node/database-cleaner/src/main.js
+++ node/database-cleaner/src/main.js
@@ -12,7 +12,7 @@ export default async ({ req, res, log, error }) => {
 
   const appwrite = new AppwriteService();
 
-  const collections = await this.listAllCollections(process.env.APPWRITE_DATABASE_ID);
+  const collections = await appwrite.listAllCollections(process.env.APPWRITE_DATABASE_ID);
 
   for (const collection of collections) {
     await this.cleanCollection(process.env.APPWRITE_DATABASE_ID, collection.$id);
