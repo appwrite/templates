f5b0f83ae75a282fb391f9174e8c61d860fcd648
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2023-10-26 11:53:36 -0500

    remove logs

--- node/database-cleaner/src/main.js
+++ node/database-cleaner/src/main.js
@@ -2,8 +2,6 @@ import AppwriteService from './appwrite.js';
 import { throwIfMissing } from './utils.js'
 
 export default async ({ req, res, log, error }) => {
-  log(process.env)
-
   throwIfMissing(process.env, [
     'APPWRITE_DATABASE_ID',
     'APPWRITE_API_KEY',
@@ -13,8 +11,6 @@ export default async ({ req, res, log, error }) => {
   const appwrite = new AppwriteService();
 
   const collections = await appwrite.listAllCollections(process.env.APPWRITE_DATABASE_ID);
-
-  log(collections)
 
   for (const collection of collections) {
     await appwrite.cleanCollection(process.env.APPWRITE_DATABASE_ID, collection.$id);
