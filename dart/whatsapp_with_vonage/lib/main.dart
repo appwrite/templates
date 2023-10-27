370ce34a5cd23b7754c53ffe3a219e9a39d215a1
Tejas Raskar <tjsrskr@gmail.com>
2023-10-27 16:43:28 +0530

    used JSON for error handling

--- dart/whatsapp_with_vonage/lib/main.dart
+++ dart/whatsapp_with_vonage/lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:crypto/crypto.dart';
 import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:whatsapp_with_vonage/utils.dart';
@@ -16,37 +15,34 @@ Future<dynamic> main(final context) async {
 
   if (context.req.method == 'GET') {
     return context.res.send(getStaticFile('index.html'), 200,
-        {'Content-Type': 'text/html; character=utf-8'});
+        {'Content-Type': 'text/html; charset=utf-8'});
   }
 
   final token = context.req.headers['authorization'].split(' ')[1];
 
-  final apiSecretKey = Platform.environment['VONAGE_API_SIGNATURE_SECRET'];
-
   if (token == null) {
-    return context.res.send('Unauthorized', 401);
+    return context.res.json({'ok': false, 'error': 'Unauthorized'}, 401);
   }
 
   if (context.req.body['from'] == null || context.req.body['text'] == null) {
-    return context.res.send("Missing required fields.", 400);
+    return context.res.json({'ok': false, 'error': 'Missing required fields.'}, 400);
   }
 
-  final jwt = JWT.verify(token, SecretKey(apiSecretKey!));
+  final jwt = JWT.verify(token, SecretKey(Platform.environment['VONAGE_API_SIGNATURE_SECRET']!));
 
   if (jwt.payload['payload_hash'] == null) {
-    return context.res.send('Missing payload hash', 400);
+    return context.res.json({'ok': false, 'error': 'Missing payload hash.'}, 400);
   }
 
   final payloadHash = sha256.convert(utf8.encode(context.req.bodyRaw)).toString();
 
   if (jwt.payload['payload_hash'] != payloadHash) {
-    return context.res.send('Payload Hash Mismatch', 401);
+    return context.res.json({'ok': false, 'error': 'Payload hash mismatch.'}, 401);
   }
 
-  final basicAuthToken = base64Encode(utf8.encode(
-      '${Platform.environment['VONAGE_API_KEY']}:${Platform.environment['VONAGE_API_SECRET']}'));
+  final basicAuthToken = base64Encode(utf8.encode('${Platform.environment['VONAGE_API_KEY']}:${Platform.environment['VONAGE_API_SECRET']}'));
 
-  final response = await http.post(
+  await http.post(
     Uri.parse('https://messages-sandbox.nexmo.com/v1/messages'),
     headers: {
       'Content-Type': 'application/json',
@@ -61,5 +57,5 @@ Future<dynamic> main(final context) async {
     }),
   );
 
-  return context.res.send("Success", 200);
+  return context.res.json({'ok': true});
 }
