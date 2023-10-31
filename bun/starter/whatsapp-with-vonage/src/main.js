4ba3f84dc4949c0138358fc2724186bd3ce430ae
parthj000 <146522413+parthj000@users.noreply.github.com>
2023-11-01 00:55:40 +0530

    Update main.js

--- bun/starter/whatsapp-with-vonage/src/main.js
+++ bun/starter/whatsapp-with-vonage/src/main.js
@@ -1,64 +1,69 @@
-import express from "express"
-import jwt from "jsonwebtoken"
-import path from "path"
-
-const app = express();
-app.use(express.json())
-app.listen(PORT, () => console.log("ready"))
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve("public/index.html"))
-
-})
+import jwt from 'jsonwebtoken';
 
 
-const secret = `${Bun.env.VONAGE_API_KEY}:${Bun.env.VONAGE_ACCOUNT_SECRET}`;
-const basicAuthToken = btoa(secret);
+export default async ({ req, res, log, error }) => {
 
+    const html = file("public/index.html")
+    if (req.method === "GET") {
+        return res.send(html, {
+            satus: 200,
+            headers: {
+                "Content-Type": "text/html",
+            },
+        });
 
-
-function verify(req, res, next) {
-  const authHeader = req.headers.authorization.split(' ')[1];
-  jwt.verify(authHeader, Bun.env.SIGNATURE_SECRET, { algorithms: ['HS256'] }, (err, response) => {
-
-    if (err) {
-       return response.json({
-                status:'400',
-                message:"couldn't verify"
-            });
     }
 
-    next();
-  })
+
+
+   if (req.method === "POST") {
+        const secret = `${Bun.env.VONAGE_API_KEY}:${Bun.env.VONAGE_API_SECRET}`;
+        const basicAuthToken = btoa(secret);
+        const authHeader = req.headers.authorization.split(' ')[1];
+        jwt.verify(authHeader, process.env.SIGNATURE_SECRET, { algorithms: ['HS256'] }, (error, response) => {
+
+            if (error) {
+                return response.json({
+                    ok: false,
+                    error: "can't verify"
+
+                }, 400);
+            }
+
+
+        })
+
+        const myHeaders = new Headers();
+        myHeaders.append("Accept", "application/json");
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Authorization", `Basic ${basicAuthToken}`);
+
+        const raw = JSON.stringify({
+            from: `${Bun.env.FROM_NUMBER}`,
+            to: `${req.body.from}`,
+            message_type: "text",
+            text: `you sent me: ${req.body.text}`,
+            channel: "whatsapp"
+        });
+
+        const requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+
+        };
+        if (!(req.body.text == null)) {
+            await fetch("https://messages-sandbox.nexmo.com/v1/messages", requestOptions)
+        }
+        res.json({
+            ok: true
+
+
+
+        }, 200)
+    }
+
+
 
 }
 
-
-app.post("/", verify, async (req, res) => {
-  const myHeaders = new Headers();
-  myHeaders.append("Accept", "application/json");
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append("Authorization", `Basic ${basicAuthToken}`);
-
-  const raw = JSON.stringify({
-    from: `${Bun.env.FROM_NUMBER}`,
-    to: `${req.body.from}`,
-    message_type: "text",
-    text: `you sent me: ${req.body.text}`,
-    channel: "whatsapp"
-  });
-
-  const requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-
-  };
-  if (!(req.body.text == null)) {
-    await fetch("https://messages-sandbox.nexmo.com/v1/messages", requestOptions)
-  }
-  res.json({
-      status:'200',
-      message: req.body.status
- })
-})
