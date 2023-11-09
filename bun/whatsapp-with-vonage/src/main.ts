4549b5d5a734b359941556da18e654440343cd02
Parth <parsjoshi2@gmail.com>
2023-11-10 04:26:10 +0530

    commited changes

--- bun/whatsapp-with-vonage/src/main.ts
+++ bun/whatsapp-with-vonage/src/main.ts
@@ -1,44 +1,50 @@
 import jwt from "jsonwebtoken";
 import { fetch } from "undici";
-import { getStaticFiles } from "./utils";
+import { getStaticFiles, throwIfMissing } from "./utils.ts";
 
-export default async ({ req, res, log, error }) => {
+export default async ({ req, res, log, error }: any) => {
+  throwIfMissing(Bun.env, [
+    "VONAGE_API_KEY",
+    "VONAGE_ACCOUNT_SECRET",
+    "VONAGE_WHATSAPP_NUMBER",
+    "VONAGE_SIGNATURE_SECRET",
+  ]);
+
   if (req.method === "GET") {
-    return res.send(getStaticFiles, 200, {
+    return res.send(getStaticFiles("index.html"), 200, {
       "Content-Type": "text/html; charset=utf-8",
     });
   }
-
-  const hasher = new Bun.CryptoHasher("sha256");
-  const hashedBuffer = hasher.update(JSON.stringify(req.body)).digest("hex");
-  const hashedValue = hashedBuffer.toString();
-
-  const authHeader = req.headers.authorization.split(" ")[1];
+  const authHeader: string = (req.headers.authorization ?? "").split(" ")[1];
   const decodedToken = jwt.verify(authHeader, Bun.env.VONAGE_SIGNATURE_SECRET, {
     algorithms: ["HS256"],
   });
 
-  if (hashedValue != decodedToken["payload_hash"]) {
-    return res.json(
-      {
-        ok: false,
-        error: "Payload hash mismatched",
-      },
-      401
-    );
+  try {
+    throwIfMissing(decodedToken, ["payload_hash"]);
+  } catch (err) {
+    return res.json({ ok: false, error: err.message }, 400);
   }
 
-  const secret = `${Bun.env.VONAGE_API_KEY}:${Bun.env.VONAGE_ACCOUNT_SECRET}`;
-  const basicAuthToken = btoa(secret);
+  const hasher = new Bun.CryptoHasher("sha256");
+  const hashedValue = hasher.update(req.bodyRaw).digest("hex").toString();
+
+  if (hashedValue != decodedToken["payload_hash"]) {
+    return res.json({ ok: false, error: "Payload hash mismatched" }, 401);
+  }
+
+  try {
+    throwIfMissing(req.body, ["from", "text"]);
+  } catch (err) {
+    return res.json({ ok: false, error: err.message }, 400);
+  }
+
+  const basicAuthToken: string = btoa(
+    `${Bun.env.VONAGE_API_KEY}:${Bun.env.VONAGE_ACCOUNT_SECRET}`
+  );
 
   if (req.body.text == null) {
-    return res.json(
-      {
-        ok: true,
-        status: req.body.status,
-      },
-      200
-    );
+    return res.json({ ok: true, status: req.body.status }, 200);
   }
 
   await fetch(`https://messages-sandbox.nexmo.com/v1/messages`, {
@@ -56,10 +62,5 @@ export default async ({ req, res, log, error }) => {
     },
   });
 
-  return res.json(
-    {
-      ok: true,
-    },
-    200
-  );
+  return res.json({ ok: true }, 200);
 };
--- bun/whatsapp-with-vonage/src/utils.ts
+++ bun/whatsapp-with-vonage/src/utils.ts
@@ -1,5 +1,21 @@
-export async function getStaticFiles() {
-  const fileName = Bun.resolveSync("../static/index.html", import.meta.dir);
-  const html = await Bun.file(fileName).text();
+// Throws an error if any of the keys are missing from the object
+
+export async function getStaticFiles(fileName: string) {
+  const filePath = Bun.resolveSync(`../static/${fileName}`, import.meta.dir);
+  const html = await Bun.file(filePath).text();
   return html;
+}
+
+//  Returns the contents of a file in the static folder
+
+export function throwIfMissing(object: any, keys: string[]) {
+  const missing: string[] = [];
+  for (let key of keys) {
+    if (!(key in object) || !object[key]) {
+      missing.push(key);
+    }
+  }
+  if (missing.length > 0) {
+    throw new Error(`Parameters are missing :${missing.join(", ")}`);
+  }
 }
