ee820caa17a00dc624c90ee4b3add53979ed4c49
loks0n <22452787+loks0n@users.noreply.github.com>
2024-04-02 10:05:52 +0100

    chore: speak with elevenlabs

--- node/speak-with-elevenlabs/src/main.js
+++ node/speak-with-elevenlabs/src/main.js
@@ -1,8 +1,16 @@
 import { getStaticFile, throwIfMissing } from "./utils.js";
-import { Client, Storage, ID, InputFile, Permission, Role } from "node-appwrite";
-import fetch from "node-fetch";
+import {
+  Client,
+  Storage,
+  ID,
+  InputFile,
+  Permission,
+  Role,
+} from "node-appwrite";
+import { ElevenLabsClient } from "elevenlabs";
+import consumers from "stream/consumers";
 
-export default async ({ req, res, error }) => {
+export default async ({ req, res }) => {
   throwIfMissing(process.env, [
     "ELEVENLABS_API_KEY",
     "APPWRITE_API_KEY",
@@ -15,48 +23,41 @@ export default async ({ req, res, error }) => {
     });
   }
 
-  const client = new Client()
-    .setEndpoint(process.env.APPWRITE_ENDPOINT ?? "https://cloud.appwrite.io/v1")
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(process.env.APPWRITE_API_KEY);
-
   if (!req.body.text || typeof req.body.text !== "string") {
     return res.json({ ok: false, error: "Missing required field `text`" }, 400);
   }
 
-  const body = {
-    accent: req.body.accent || "british",
+  const elevenlabs = new ElevenLabsClient();
+
+  const speechAudio = await elevenlabs.voiceGeneration.generate({
+    accent: req.body.accent ?? "british",
     accent_strength: 1.0,
-    age: req.body.age || "young",
-    gender: req.body.gender || "female",
+    age: req.body.age ?? "young",
+    gender: req.body.gender ?? "female",
     text: req.body.text,
-  };
+  });
+  const blob = await consumers.blob(speechAudio);
 
-  const resp = await fetch(
-    "https://api.elevenlabs.io/v1/voice-generation/generate-voice",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "xi-api-key": process.env.ELEVENLABS_API_KEY,
-      },
-      body: JSON.stringify(body),
-    },
-  );
+  const client = new Client()
+    .setEndpoint(
+      process.env.APPWRITE_ENDPOINT ?? "https://cloud.appwrite.io/v1"
+    )
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(process.env.APPWRITE_API_KEY);
 
-  if (resp.status !== 200) {
-    error(await resp.text());
-    return res.json({ ok: false, error: "Failed to generate audio" }, 500);
-  }
-
-  // Upload audio file to Appwrite Storage
   const storage = new Storage(client);
-  let file = await storage.createFile(
+  const file = await storage.createFile(
     process.env.APPWRITE_BUCKET_ID,
     ID.unique(),
-    InputFile.fromBlob(await resp.blob(), "audio.mp3"),
-    [Permission.read(Role.any())],
+    InputFile.fromBlob(blob, "audio.mp3"),
+    [Permission.read(Role.any())]
   );
 
-  return res.json({ ok: true, response: `${endpoint}/storage/buckets/${process.env.APPWRITE_BUCKET_ID}/files/${file['$id']}/view?project=${process.env.APPWRITE_FUNCTION_PROJECT_ID}` }, 200);
+  return res.json(
+    {
+      ok: true,
+      response: `${endpoint}/storage/buckets/${process.env.APPWRITE_BUCKET_ID}/files/${file["$id"]}/view?project=${process.env.APPWRITE_FUNCTION_PROJECT_ID}`,
+    },
+    200
+  );
 };
