7f01d1038fffb093052ec7775cdbbd01de9b2b36
adityaoberai <adityaoberai1@gmail.com>
2024-04-09 18:44:29 +0530

    Add payments with lemon squeezy template

---
+++ node/payments-with-lemon-squeezy/src/appwrite.js
@@ -0,0 +1,97 @@
+import { Client, Databases, ID, Permission, Role } from 'node-appwrite';
+
+class AppwriteService {
+  constructor() {
+    const client = new Client();
+    client
+      .setEndpoint(
+        process.env.APPWRITE_ENDPOINT ?? 'https://cloud.appwrite.io/v1'
+      )
+      .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+      .setKey(process.env.APPWRITE_API_KEY);
+
+    this.databases = new Databases(client);
+
+    this.setup();
+  }
+
+  async setup() {
+    const databaseId = process.env.APPWRITE_DATABASE_ID ?? 'orders';
+    const collectionId = process.env.APPWRITE_COLLECTION_ID ?? 'orders';
+
+    if ((await this.doesOrdersDatabaseExist(databaseId)) == true) {
+      return;
+    }
+
+    await this.setupOrdersDatabase(databaseId, collectionId);
+  }
+
+  async doesOrdersDatabaseExist(databaseId) {
+    try {
+      await this.databases.get(databaseId);
+      return true;
+    } catch (err) {
+      if (err.code !== 404) throw err;
+      return false;
+    }
+  }
+
+  async setupOrdersDatabase(databaseId, collectionId) {
+    try {
+      await this.databases.create(databaseId, 'Orders Database');
+    } catch (err) {
+      if (err.code !== 409) throw err;
+    }
+
+    try {
+      await this.databases.createCollection(
+        databaseId,
+        collectionId,
+        'Orders Collection',
+        undefined,
+        true
+      );
+    } catch (err) {
+      if (err.code !== 409) throw err;
+    }
+
+    try {
+      await this.databases.createStringAttribute(
+        databaseId,
+        collectionId,
+        'userId',
+        255,
+        true
+      );
+    } catch (err) {
+      if (err.code !== 409) throw err;
+    }
+
+    try {
+      await this.databases.createStringAttribute(
+        databaseId,
+        collectionId,
+        'orderId',
+        255,
+        true
+      );
+    } catch (err) {
+      if (err.code !== 409) throw err;
+    }
+  }
+
+  async createOrder(databaseId, collectionId, userId, orderId) {
+    await this.databases.createDocument(
+      databaseId,
+      collectionId,
+      ID.unique(),
+      {
+        userId,
+        orderId,
+      },
+      [Permission.read(Role.user(userId))]
+    );
+  }
+}
+
+export default AppwriteService;
