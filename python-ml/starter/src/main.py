3b6e4c731b9d4456a0a35ca70c0cc740673b3bf6
loks0n <22452787+loks0n@users.noreply.github.com>
2024-06-17 13:55:09 +0100

    feat: python ml starter

---
+++ python-ml/starter/src/main.py
@@ -0,0 +1,39 @@
+from appwrite.client import Client
+import os
+import numpy
+
+
+# This is your Appwrite function
+# It's executed each time we get a request
+def main(context):
+    # Why not try the Appwrite SDK?
+    #
+    # client = (
+    #     Client()
+    #     .set_endpoint("https://cloud.appwrite.io/v1")
+    #     .set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"])
+    #     .set_key(os.environ["APPWRITE_API_KEY"])
+    # )
+
+    # You can log messages to the console
+    context.log("Hello, Logs!")
+
+    # If something goes wrong, log an error
+    context.error("Hello, Errors!")
+
+    # The `ctx.req` object contains the request data
+    if context.req.method == "GET":
+        # Send a response with the res object helpers
+        # `ctx.res.send()` dispatches a string back to the client
+        return context.res.send("Hello, World!")
+
+    # `ctx.res.json()` is a handy helper for sending JSON
+    return context.res.json(
+        {
+            "motto": "Build like a team of hundreds_",
+            "learn": "https://appwrite.io/docs",
+            "connect": "https://appwrite.io/discord",
+            "getInspired": "https://builtwith.appwrite.io",
+            "numpy": numpy.__version__,
+        }
+    )
