c420beb0f777e0f836d75e343f686a18a1a4731c
Matej Bačo <matejbaco2000@gmail.com>
2024-08-01 07:04:43 +0000

    improe starter copy

--- bun/starter/src/main.ts
+++ bun/starter/src/main.ts
@@ -3,7 +3,7 @@ import { Client, Users } from "node-appwrite";
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }: any) => {
   // You can use the Appwrite SDK to interact with other services
-  // For this example, we're using the Auth service
+  // For this example, we're using the Users service
   const client = new Client()
     .setEndpoint(Bun.env["APPWRITE_FUNCTION_API_ENDPOINT"])
     .setProject(Bun.env["APPWRITE_FUNCTION_PROJECT_ID"])
@@ -14,14 +14,14 @@ export default async ({ req, res, log, error }: any) => {
     const response = await users.list();
     // Log messages and errors to the Appwrite Console
     // These logs won't be seen by your end users
-    log(`Amount of users: ${response.total}`);
+    log(`Total users: ${response.total}`);
   } catch(err) {
     error("Could not list users: " + err.message);
   }
 
-  // The `req` object contains the request data
+  // The req object contains the request data
   if (req.path === "/ping") {
-    // Use `res` object to respond with `text()`, `json()`, or `binary()`
+    // Use res object to respond with text(), json(), or binary()
     // Don't forget to return a response!
     return res.text("Pong");
   }
